a0f1870d1848928f14046aed24f39c0d7f4eab99
santiagoandre <saramirez@unicauca.edu.co>
2019-06-01 15:21:21 -0500

    Primera version de la plantilla de materias

---
+++ materias.php
@@ -0,0 +1,32 @@
+<html>
+	<head>
+		<link rel="stylesheet" href="css/materias.css" />
+	</head>
+	<body>
+        <div class="container">
+
+			    <div id = "materias">
+			      <div class="semestre">
+							<h5>semestre 1</h5>
+
+						</div>
+						<div class="semestre">
+							<h5>semestre 2</h5>
+
+						</div>
+						<div class="semestre">
+							<h5>semestre 3</h5>
+
+						</div>
+						<div class="semestre">
+							<h5>semestre 4</h5>
+						</div>
+						<div class="semestre">
+							<h5>semestre 5</h5>
+						</div>
+			    </div>
+
+        </div>
+
+</body>
+</html>
