0b5b5f7ec2e868bf0412105272b06520cf585cdb
santiagoandre <saramirez@unicauca.edu.co>
2019-06-01 16:33:44 -0500

    Plantilla final de las materias

---
+++ plantillaMaterias.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="css/materias.css" />
+		<title>Administrador Pensum</title>
+	</head>
+	<body>
+
+	</body>
+</html>
+<div class="content">
+
+    <div class="semestre">
+      <h1 class="titulo_semestre" data-name="Lorem">Semestre 1</h1>
+			<div class="materia">Calculo 1</div>
+			<div class="materia">Calculo 2</div>
+			<div class="materia">Calculo 3</div>
+			<div class="materia">Calculo 4</div>
+    </div>
+    <div class="semestre1">
+      <h1 class="titulo_semestre" data-name="Ipsum">Semestre 2</h1>
+			<div class="materia">Calculo 1</div>
+			<div class="materia">Calculo 2</div>
+			<div class="materia">Calculo 3</div>
+			<div class="materia">Calculo 4</div>
+    </div>
+    <div class="semestre2">
+      <h1 class="titulo_semestre" data-name="Dolor">Semestre 3</h1>
+			<div class="materia">Calculo 1</div>
+			<div class="materia">Calculo 4</div>
+    </div>
+		<div class="semestre3">
+      <h1 class="titulo_semestre" data-name="Sit">Semestre 4</h1>
+			<div class="materia">Calculo 1</div>
+			<div class="materia">Calculo 3</div>
+			<div class="materia">Calculo 4</div>
+
+    </div>
+		<div class="semestre4">
+			<h1 class="titulo_semestre" data-name="Sit">Semestre 4</h1>
+			<div class="materia">Calculo 1</div>
+			<div class="materia">Calculo 3</div>
+			<div class="materia">Calculo 4</div>
+
+		</div>
+
+  <i class="fa fa-close strip__close"></i>
+</section>
