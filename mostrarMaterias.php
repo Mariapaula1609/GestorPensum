6df528110c7ef8a3ccc49c1d12c31ed72a76e200
santiago <santidelacruz@unicauca.edu.co>
2019-06-01 22:20:51 -0500

    Se organizaron los json en una carpeta persistencia

--- mostrarMaterias.php
+++ mostrarMaterias.php
@@ -1,31 +1,22 @@
 <?php
 //inclye el archivo php porque tiene ula clase
 include 'Materias.php';
-// Read JSON file
-//carga en $json lo que tiene el archivo carts.json
-$json = file_get_contents('pensum.json');
-//Decode JSON
-//crea un carta de a pair de los datos del json
-//es es un parce//
-// un arreglo de cartas!
+$json = file_get_contents('persistencia/pensum.json');
 $materias = json_decode($json,true);
-//crea una carta apatir del vecot de cartaSnippet
-//$c = Materia::createFromArray($materia[0]);
-//si existe el objeto carta enviado por get
 if(isset($_GET["codigo"]))
 {    // el codigo de la materia es n(?)
     $codigo=$_GET["codigo"];
-    $nombreArchivo=$codigo.".json";
+    $nombreArchivo="persistencia/".$codigo.".json";
     if(file_exists($nombreArchivo)){
     //  echo  "lo encontro!";
     }else{
-       if(copy("pensum.json",$nombreArchivo)){
+       if(copy("persistencia/pensum.json",$nombreArchivo)){
       //   echo "archivo creado";
        }else {
         // echo "Ah ocurrido un error en el sistema, contacte al adminitrador";
        }
-       echo file_get_contents($nombreArchivo);
     }
+    echo file_get_contents($nombreArchivo);
 }
 else
 {
